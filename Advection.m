75756b19dcd8d49f61f80d3bb8532e24b8c94400
amandasingleton <amandasingleton36@gmail.com>
2017-05-31 14:27:13 -0700

    Add files via upload

--- Advection.m
+++ Advection.m
@@ -4,8 +4,7 @@ N=100;M=100;
 center=[1 0];
 r=0.25;
 rho_0=zeros(N,M);
-rho_x=zeros(N,M);
-rho_y=zeros(N,M);
+
 x=linspace(-pi/2,pi/2,N);
 y=linspace(-pi/2,pi/2,M);
 
@@ -19,55 +18,59 @@ end
 
 %initial conditions for numerical approximation
 
-t=0; tfinal=50;
+t=0; tfinal=pi;
 dx=x(2)-x(1); dy=y(2)-y(1); dt=0.2*dx;
 rho=rho_0;
+rho_x=zeros(N,M);
+rho_y=zeros(N,M);
 
 while t<tfinal
     if t+dt>tfinal
         dt=tfinal-t;
     end
     
-    for i=2:N-1
-        for j=2:M-1
+    for i=1:N %looping over the grid
+        for j=1:M
+            %set the velocity for particular (i,j,t)
             c1=-cos(x(i))*sin(y(j))*cos(t);
             c2=sin(x(i))*cos(y(j))*cos(t);
+            %boundary conditions
             if( i==1 || i==N || j==1 || j==M )
                 rho(i,j)=0;
+            else
+                    
+                if c1>0 
+                    rho_x(i,j)=(rho_0(i,j)-rho_0(i-1,j));
+                        if c2>0
+                             rho_y(i,j)=(rho_0(i,j)-rho_0(i,j-1));
+
+                        else
+                             rho_y(i,j)=(rho_0(i,j+1)-rho_0(i,j));
+
+                        end
+
+                else 
+                    rho_x(i,j)=(rho_0(i+1,j)-rho_0(i,j));
+                        if c2>0
+                             rho_y(i,j)=(rho_0(i,j)-rho_0(i,j-1));
+
+                        else
+                             rho_y(i,j)=(rho_0(i,j+1)-rho_0(i,j));
+
+                        end
+                 end
             end
-            if c1>0 
-                rho_x(i,j)=(rho_0(i,j)-rho_0(i-1,j));
-                    if c2>0
-                         rho_y(i,j)=(rho_0(i,j)-rho_0(i,j-1));
-                         
-                    else
-                         rho_y(i,j)=(rho_0(i,j+1)-rho_0(i,j));
-                         
-                    end
-                                    
-            elseif c1<0 
-                rho_x(i,j)=(rho_0(i+1,j)-rho_0(i,j));
-                    if c2>0
-                         rho_y(i,j)=(rho_0(i,j)-rho_0(i,j-1));
-                        
-                    else
-                         rho_y(i,j)=(rho_0(i,j+1)-rho_0(i,j));
-                         
-                    end
-            end
-           
+            
+            %THIS IS THE APPROXIMATION
             rho(i,j)=rho_0(i,j)-c1*dt/dx*rho_x(i,j)-c2*dt/dy*rho_y(i,j);
-              
                        
         end
     end
     
-    %prepre for next time step
+    %prepare for next time step
     rho_0=rho; t=t+dt;
-        
+    %plot rho    
     mesh(x,y,rho);
     axis([-2 2 -2 2 0 1.1]);
-    xlabel('x');
-    %view(0, 90);
     pause(dt);
 end
